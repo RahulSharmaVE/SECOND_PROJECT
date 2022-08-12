6e0a269699111bea7bfc205a2ce6a328ab14f80b
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-12 12:28:35 +0530

    UTF7 Encoding Example.

---
+++ SECONDPROJECT/UTF7Encode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECONDPROJECT
+{
+   class UTF7Encode
+    {
+        static void Main(string[] args)
+        {
+            UTF7Encoding encode = new UTF7Encoding();//Creating a UTF7Encoding Object
+            Console.WriteLine("Enter the string : ");
+            string s = Console.ReadLine();//Input from the user
+            Console.WriteLine();
+            Console.WriteLine("Original string is : ");
+            Console.WriteLine(s); //display of the original string
+            byte[] bytes = encode.GetBytes(s); //Encoding the string into bytes
+            Console.WriteLine();
+            Console.Write("Encoded Bytes are : ");
+            foreach (byte b in bytes)
+            {
+                Console.WriteLine(b);//Encoded bytes output
+            }
+            Console.WriteLine();
+            string decodestring= encode.GetString(bytes);//Decode the bytes back into the string 
+            Console.WriteLine("Decoded Bytes are : ");
+            Console.WriteLine(decodestring);
+        }
+    }
+}
